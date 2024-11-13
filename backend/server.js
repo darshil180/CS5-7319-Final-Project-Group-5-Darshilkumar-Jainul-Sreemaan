7af3e67950238b687eceb54f60e22e22ad1ce5e1
darshil180 <darshilkitaliya@gmail.com>
2024-11-13 12:07:11 -0600

    Add Orders api and models

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/database');
 const authRoutes = require('./routes/auth'); // Import authentication routes
 const menuRoutes = require('./routes/menu');
 const cartRoutes = require('./routes/cart');
+const orderRoutes = require('./routes/orders');
 const responseFormatter = require('./middleware/responseFormatter');
 
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(responseFormatter); // Apply response formatter middleware globally
 app.use('/api/auth', authRoutes); // Route for authentication-related endpoints
 app.use('/api/menu', menuRoutes);  // Menu routes
 app.use('/api/cart', cartRoutes);  // Cart routes
+app.use('/api/orders', orderRoutes);
 
 // Example route for testing
 app.get('/', (req, res) => {
